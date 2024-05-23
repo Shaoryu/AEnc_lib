3ae17f4c6877356407c77cb5a87cd2f436c03cda
Shaoryu <149553583+Shaoryu@users.noreply.github.com>
2024-05-23 17:51:42 +0900

    Update aenc.h

--- aenc.h
+++ aenc.h
@@ -5,17 +5,14 @@
 #include "CANMotor.h"
 
 
-class aenc : public SPI
+class aenc 
 {
 public:
-    
-    
-    aenc(PinName mosi,PinName miso,PinName clk,PinName cs);//miso(DO),clk(CLK),cs(CSn)※mosiは使っていないよ;
-    aenc(PinName mosi,PinName miso,PinName clk,PinName cs,int target);//miso(DO),clk(CLK),cs(CSn)※mosiは使っていないよ;
+    aenc(SPI &spi,PinName cs,int target);//miso(DO),clk(CLK),cs(CSn)※mosiは使っていないよ;
+    aenc(SPI &spi,PinName cs);//miso(DO),clk(CLK),cs(CSn)※mosiは使っていないよ;
     void set_target(int target);
-    float KP,KI,KD;//pidのゲイン。
     static float duty;//デューティ比。マックス0.3,最小0.01よりちっちゃい
-    float angle_point;//目標(deg)。
+    static float angle_point;//目標(deg)。
 
     
     float get_data();
@@ -25,7 +22,7 @@ public:
     static int state;
 
 private:
-    
+    SPI &SPi;
     DigitalOut _cs;
 
     float res;
@@ -35,6 +32,7 @@ private:
     float i;
     float last_diff;
     float d;
+    float KP,KI,KD;//pidのゲイン。
     enum{
         Free,
         CW,
